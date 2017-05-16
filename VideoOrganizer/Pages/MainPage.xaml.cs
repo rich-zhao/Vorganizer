11488a007ee1f348d8c19dac50757cf925b5b59a
Richard Zhao <xxanimus@gmail.com>
2017-05-15 20:47:37 -0400

    Cleaned up commented code, added MediaInfo library

--- VideoOrganizer/Pages/MainPage.xaml.cs
+++ VideoOrganizer/Pages/MainPage.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using MediaToolkit;
+using MediaToolkit.Model;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
@@ -30,42 +33,20 @@ namespace VideoOrganizer
                 //TODO: Timer this
                 Dictionary<string, string> propertiesDict = new Dictionary<string, string>();
                 List<string> arrHeaders = new List<string>();
-                Shell32.Shell shell = new Shell32.Shell();
-                Shell32.Folder objFolder;
 
                 //gets the path of drag and drop file
                 String[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
                 foreach (String i in files){
-                    string file = i.Split('\\').Last();
-                    string path = i.Replace(file, "");
-              
-                    objFolder = shell.NameSpace(@path);
-                    Shell32.ShellFolderItem item = (Shell32.ShellFolderItem)objFolder.Items().Item(file);
-                    string prop = item.ExtendedProperty("{64440491-4C8B-11D1-8B70-080036B11A03}");
-                    /*
-                    //gets extended properties' headers
-                    for (int j=0; j<short.MaxValue; j++)
-                    {
-                        string header = objFolder.GetDetailsOf(null, j);
-                        if (String.IsNullOrEmpty(header))
-                            continue;
-                        arrHeaders.Add(header);
-                        propertiesDict[header] = "";
-                    }
+                    long size = new FileInfo(i).Length;
 
-                    //set actual file properties
-                    for(int j=0; j<arrHeaders.Count; j++)
+                    var inputFile = new MediaFile {Filename = @i };
+                    using (var engine = new Engine())
                     {
-                        string header = arrHeaders[j];
-                        string value = objFolder.GetDetailsOf(objFolder.ParseName(file), j);
-                        propertiesDict[arrHeaders[j]] = objFolder.GetDetailsOf(objFolder.ParseName(file), j);
+                        engine.GetMetadata(inputFile);
                     }
-                    */
                     Console.WriteLine(propertiesDict);
                     
                 }
-                //DirectoryInfo di = new DirectoryInfo();
-                //FileInfo fi = new FileInfo();
                 Console.WriteLine("test");
                // dragNdrop.Content = e.Data.GetData(DataFormats.FileDrop);
             }
