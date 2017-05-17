51a4d83e43c7b7d1620fba40b38b3b515f2c5a3a
Richard Zhao <xxanimus@gmail.com>
2017-05-16 22:26:40 -0400

    Code to initialize new DB tables

--- VideoOrganizer/Service/DatabaseService.cs
+++ VideoOrganizer/Service/DatabaseService.cs
@@ -11,6 +11,8 @@ namespace VideoOrganizer
     public class DatabaseService
     {
         private static DatabaseService instance;
+        private static SQLiteConnection connection;
+
         private DatabaseService() { }
 
         public static DatabaseService Instance
@@ -25,11 +27,32 @@ namespace VideoOrganizer
             }
         }
 
+        /// <summary>
+        /// Initializes a new Database according to the path the user has shown in the SaveFileDialog
+        /// </summary>
+        /// <param name="path">Absolute file path provided by SaveFileDialog</param>
         public void initializeNewDb(string path)
         {
-            
             SQLiteConnection.CreateFile(path);
+            connection = new SQLiteConnection("Data Source=" + path + ";Version=3;");
+            connection.Open();
+            string videoQuery = "CREATE TABLE videos (id int, name text, path text, is_favorite integer, " +
+                "file_size text, play_count integer, rating integer, resolution text, seconds integer, " +
+                "hash text, date_added datetime, date_original datetime, date_last_watched datetime)";
+            SQLiteCommand command = new SQLiteCommand(videoQuery, connection);
+            command.ExecuteNonQuery();
 
+            string videoTagQuery = "CREATE TABLE video_tags(id int, video_id int, tag_id int)";
+            command = new SQLiteCommand(videoTagQuery, connection);
+            command.ExecuteNonQuery();
+
+            string tagQuery = "CREATE TABLE tags(id int, name text, category int)";
+            command = new SQLiteCommand(tagQuery, connection);
+            command.ExecuteNonQuery();
+
+            string categoriesQuery = "CREATE TABLE categories(id int, name text)";
+            command = new SQLiteCommand(categoriesQuery, connection);
+            command.ExecuteNonQuery();
         }
 
     }
